60423912e52d511f8050b211cfaa1c148b79eaed
jainabhishek-Full-Stack-Developer <jainabhi039@gmail.com>
2023-05-30 07:42:50 +0000

    welcome app

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,33 @@
+import {Component} from 'react'
+import './index.css'
+
+class Welcome extends Component {
+  state = {isSubscribed: false}
+
+  buttonText = () => {
+    this.setState(prevState => ({isSubscribed: !prevState.isSubscribed}))
+  }
+
+  getButtonText = () => {
+    const {isSubscribed} = this.state
+
+    return isSubscribed ? 'Subscribed' : 'Subscribe'
+  }
+
+  render() {
+    const buttonText = this.getButtonText()
+
+    return (
+      <>
+        <div className="bgContainer">
+          <h1 className="heading">Welcome</h1>
+          <p className="para">Thank you! Happy Learning</p>
+          <button type="button" className="btn" onClick={this.buttonText}>
+            {buttonText}
+          </button>
+        </div>
+      </>
+    )
+  }
+}
+export default Welcome
